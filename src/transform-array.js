6a412b2c22cc5e956835167433b2004eee9a950f
starff <starff.st@gmail.com>
2020-02-17 01:21:38 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let result = [];
+    if(Array.isArray(arr)) {
+      if (arr.length == 0 ) {
+        return arr;
+      } else {
+        arr.forEach((item, index) => {
+            if (item === '--discard-next') {
+              index++;
+              index++;
+            } else if (item === '--discard-prev') {
+              if (index != 0) {
+                result.pop();
+              } 
+            } else if (item === '--double-next') {
+              result.push(arr[index + 1])
+            } else if (item === '--double-prev') {
+              result.push(arr[index - 1])
+            } else {
+              result.push(item);
+            };
+        });
+      } 
+    } else {
+      throw new Error;
+    }
+    return result;
 };
