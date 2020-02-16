a800a5fb7234467595b7702942b71ec00ebc90ce
starff <starff.st@gmail.com>
2020-02-17 01:44:15 +0300

    fix: change solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,27 @@
 module.exports = function transform(arr) {
   let result = [];
+  let helper = 0;
     if(Array.isArray(arr)) {
       if (arr.length == 0 ) {
         return arr;
       } else {
         arr.forEach((item, index) => {
-            if (item === '--discard-next') {
-              index++;
-              index++;
+            if (item === '--discard-next' ) {
+              helper = 1;
             } else if (item === '--discard-prev') {
-              if (index != 0) {
+              if (index > 0) {
                 result.pop();
-              } 
+              };
             } else if (item === '--double-next') {
-              result.push(arr[index + 1])
+              if (index < arr.length - 1) {
+                result.push(arr[index + 1]);
+              };
             } else if (item === '--double-prev') {
-              result.push(arr[index - 1])
+              if (index > 0) {
+                result.push(arr[index - 1]);
+              };
+            } else if (helper == 1) {
+              helper = 0;
             } else {
               result.push(item);
             };
