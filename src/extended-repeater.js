dec7c4a977e9140ab374ea543afd72568e079af4
starff <starff.st@gmail.com>
2020-02-17 23:42:46 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    if (options.separator == undefined) {
+      options.separator = '+';
+    };
+    if (options.addition == undefined) {
+      options.addition = '';
+    };
+    if (options.repeatTimes == undefined) {
+      options.repeatTimes = 1;
+    };
+    if (options.additionSeparator == undefined) {
+      options.additionSeparator == '';
+    } 
+    for(let i = 0; i < options.repeatTimes; ++i) {
+      result = result + str + options.addition;
+      
+      if (i != options.repeatTimes - 1) {
+        result = result + options.separator;
+        };
+      };
+      
+    return result;
 };
   
