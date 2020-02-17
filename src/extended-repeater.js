f9a3b43ae4bbd3ed3811173203ada8c9f462cdce
starff <starff.st@gmail.com>
2020-02-18 00:07:48 +0300

    fix: change solution in extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,18 +3,29 @@ module.exports = function repeater(str, options) {
     if (options.separator == undefined) {
       options.separator = '+';
     };
-    if (options.addition == undefined) {
+    if (options.addition === undefined) {
       options.addition = '';
+    };
+    if (options.addition == null) {
+      options.addition = 'null';
     };
     if (options.repeatTimes == undefined) {
       options.repeatTimes = 1;
     };
     if (options.additionSeparator == undefined) {
       options.additionSeparator == '';
-    } 
+    };
+    if (options.additionRepeatTimes == undefined) {
+      options.additionRepeatTimes = 1;
+    };
     for(let i = 0; i < options.repeatTimes; ++i) {
-      result = result + str + options.addition;
-      
+      result = result + str;
+      for(let k = 0; k < options.additionRepeatTimes; ++k) {
+        result =result + options.addition;
+        if (k != options.additionRepeatTimes - 1) {
+          result = result + options.additionSeparator;
+          };
+      };
       if (i != options.repeatTimes - 1) {
         result = result + options.separator;
         };
