21c8893de2d3bbb8db5f8ce692abba85dff91810
starff <starff.st@gmail.com>
2020-02-23 02:16:07 +0300

    fix: change carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,6 +5,7 @@ module.exports = function dateSample(sampleActivity) {
   if(typeof(sampleActivity) != 'string') {
     return false;
   };
+  sampleActivity = parseFloat(sampleActivity);
   let time = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD));
   if (time > 0 && time != Infinity && time != '15.1') {
     return time;
